4dc783ac36ade8132422ab95a19b85e887e738bc
Yurino Oga <yurino.o@latona.io>
2022-01-01 13:12:54 +0900

    release

--- SAP_API_Output_Formatter/type.go
+++ SAP_API_Output_Formatter/type.go
@@ -2,10 +2,10 @@ package sap_api_output_formatter
 
 type Header struct {
 	ToItem            string      `json:"to_Item"`
-}    
-    
-type ToItem struct {
 }
 
 type Item struct {
+}
+
+type ToItem struct {
 }
